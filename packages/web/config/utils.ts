23b761f6166093cd6bf6209375e84f159430f454
Mehran <alonephoenix90@gmail.com>
2025-09-07 18:24:59 +0330

    Revert "fix!: keplrChain currencies generation (#4162)"
    This reverts commit 5b529bb9bebdf0dbf152e1a09470852d61113089.

--- packages/web/config/utils.ts
+++ packages/web/config/utils.ts
@@ -193,11 +193,11 @@ function getKeplrCompatibleChain({
     bip44: {
       coinType: chain?.slip44 ?? 118,
     },
-    currencies: (chain.currencies ?? []).reduce<
+    currencies: (assetList?.assets ?? []).reduce<
       ChainInfoWithExplorer["currencies"]
     >((acc, asset) => {
-      const coinMinimalDenom = asset.coinMinimalDenom ?? "";
-      const displayDecimals = asset.coinDecimals;
+      const coinMinimalDenom = asset.coinMinimalDenom;
+      const displayDecimals = asset.decimals;
 
       const isCW20ContractToken =
         coinMinimalDenom
@@ -237,27 +237,27 @@ function getKeplrCompatibleChain({
         };
       }
 
-      const imageUrl = asset?.coinImageUrl ?? "";
+      const imageUrl = asset?.logoURIs?.svg ?? asset?.logoURIs?.png;
 
       acc.push({
         type: type ?? "cw20",
-        coinDenom: asset.coinDenom,
+        coinDenom: asset.symbol,
         /**
          * In Keplr ChainStore, denom should start with "type:contractAddress:denom" if it is for the token based on contract.
          */
         coinMinimalDenom: isCW20ContractToken
-          ? coinMinimalDenom + `:${asset.coinDenom}`
+          ? coinMinimalDenom + `:${asset.symbol}`
           : coinMinimalDenom,
         contractAddress: isCW20ContractToken
           ? coinMinimalDenom.split(":")[1]!
           : "",
         coinDecimals: displayDecimals,
-        coinGeckoId: asset.coinGeckoId,
+        coinGeckoId: asset.coingeckoId,
         coinImageUrl: imageUrl
-          ? getImageRelativeFilePath(imageUrl, asset.coinDenom)
+          ? getImageRelativeFilePath(imageUrl, asset.symbol)
           : undefined,
         base: asset.coinMinimalDenom,
-        // pegMechanism: asset.pegMechanism,
+        pegMechanism: asset.pegMechanism,
         gasPriceStep,
       });
       return acc;
